ae2ea5b0fed82fdd72186efba395c414c23e45bd
Mario Bogdan Iancu <mariobogdaniancu@gmail.com>
2026-03-23 14:59:21 +0200

    Implement TokenService for JWT token generation

---
+++ src/main/java/com/smartclinic/service/TokenService.java
@@ -0,0 +1,29 @@
+package com.smartclinic.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    private final String jwtSecret = "mysupersecretkeymysupersecretkey";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+}
